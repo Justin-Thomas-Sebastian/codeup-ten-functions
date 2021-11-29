e684c2b8df8a12fb7e432aba1143a555484a2e55
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-29 16:05:53 -0700

    added solutions

--- solutions.js
+++ solutions.js
@@ -20,3 +20,49 @@
  * and
  * concat
  */
+
+function isTrue(input){
+    return input === true;  
+}
+
+function isFalse(input){
+    return input === false;
+}
+
+function not(input) {
+    return !input;
+}
+
+function addOne(input){
+    return Number(input) + 1;
+}
+
+function isEven(input){
+    let inputNum = parseInt(input);
+
+    if(inputNum % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isIdentical(a, b){
+    return a === b;
+}
+
+function isEqual(a, b){
+    return a == b;
+}
+
+function or(a, b){
+    return a || b;
+}
+
+function and(a, b){
+    return a && b;
+}
+
+function concat(a, b){
+    return String(a) + String(b);
+}
